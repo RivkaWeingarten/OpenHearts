d85a4be833a73dd614fbb5dc5d0af6c726ea6184
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-09-14 18:06:33 -0400

    added schemas, added seeder post in postman doesnt work

--- open-hearts-back-end/controllers/families.js
+++ open-hearts-back-end/controllers/families.js
@@ -1,5 +1,45 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const db = require("../models");
+require("dotenv").config();
+// const families = require("../models/family_model.js");
 
-// More code here in a moment
+// router.get("/", (req, res) => {
+//     db.Family.find()
+//       .populate("donations")
+//       .then((families) => {
+//         res.render("families/index", { families });
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         res.render("error404");
+//       });
+//   });
 
-module.exports = router
+router.get("/", async (req, res) => {
+  const families = await db.Family.find();
+  res.json(families);
+});
+
+router.post("/", (req, res) => {
+  db.Family.create(req.body)
+    .then((createdFamily) => {
+      res.json(createdFamily);
+    })
+    .catch((err) => {
+      console.log("err", err);
+      res.status(500).json({ error: "An error occurred" });
+    });
+});
+
+// router.post("/", (req, res) => {
+//   db.Family.create(req.body)
+//     .then((createdFamily) => {
+//       res.json(createdFamily);
+//     })
+//     .catch((err) => {
+//       console.error("Error:", err);
+//       res.status(500).json({ error: "An error occurred" });
+//     });
+// });
+
+module.exports = router;
