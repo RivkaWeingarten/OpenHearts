87c71ce36c2e9cb89dcd639dc0b5233effa46905
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-09-15 23:22:15 -0700

    cc form styling

--- open-hearts-front-end/src/components/Donations.jsx
+++ open-hearts-front-end/src/components/Donations.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import CreditCardForm from "./CreditCardForm";
 
-function Donations() {
-  return (
-    <div>
-      <h1 className="thank-you">Thank you for supporting our families!</h1>
-      <img className='helping-hands' src={`${process.env.PUBLIC_URL}/helpinghands.jpg`} alt='helping hands' />
-      <h2 className="donation-text">
-        If you would like to make a donation that will be dispersed to our community's most immediate needs, you may lend a helping hand here. We appreciate your kindness and generosity.
-      </h2>
 
-      <CreditCardForm />
-    </div>
-  );
+function Donations () {
+return (
+    <div>
+    <h1>Thank you for supporting our familes!</h1>
+
+    <CreditCardForm />
+ 
+  </div>
+);
 }
+
 
 export default Donations;
