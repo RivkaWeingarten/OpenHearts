e811576ba820b680538bddc6b5a50be4a47a339b
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-09-19 20:08:21 -0700

    Donation receieved message added

--- open-hearts-front-end/src/components/CreditCardForm.jsx
+++ open-hearts-front-end/src/components/CreditCardForm.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function CreditCardForm() {
   const [donor, setDonor] = useState("");
   const [donationAmount, setDonationAmount] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,6 +24,7 @@ function CreditCardForm() {
 
       if (response.ok) {
         console.log("Donation submitted successfully");
+        setIsSubmitted(true);
       } else {
         console.error("Failed to submit donation");
       }
@@ -36,137 +38,135 @@ function CreditCardForm() {
 
   return (
     <div className="ccBackground">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="donor" className="form-label">
-                Donor Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                value={donor}
-                id="donor"
-                placeholder="John Doe"
-                onChange={(e) => setDonor(e.target.value)}
-                required
-              />
+      {!isSubmitted ? (
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="donor" className="form-label">
+                  Donor Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={donor}
+                  id="donor"
+                  placeholder="John Doe"
+                  onChange={(e) => setDonor(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="donationAmount" className="form-label">
+                  Donation Amount
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  value={donationAmount}
+                  id="donationAmount"
+                  placeholder="Enter Donation Amount"
+                  onChange={(e) => setDonationAmount(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="cardNumber" className="form-label">
+                  Card Number
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cardNumber"
+                  placeholder="1234 5678 9012 3456"
+                />
+              </div>
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="donationAmount" className="form-label">
-                Donation Amount
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                value={donationAmount}
-                id="donationAmount"
-                placeholder="Enter Donation Amount"
-                onChange={(e) => setDonationAmount(e.target.value)}
-                required
-              />
+          <div className="row">
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="expiryDate" className="form-label">
+                  Expiration Date
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="expiryDate"
+                  placeholder="MM/YY"
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="cvv" className="form-label">
+                  CVV
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="cvv"
+                  placeholder="123"
+                />
+              </div>
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="cardNumber" className="form-label">
-                Card Number
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cardNumber"
-                placeholder="1234 5678 9012 3456"
-                // required not real so not needed yet
-              />
-            </div>
-          </div>
-        </div>
 
-        <div className="row">
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="expiryDate" className="form-label">
-                Expiration Date
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="expiryDate"
-                placeholder="MM/YY"
-                // required
-              />
+          {/* Billing Information */}
+          <div className="input-group">
+            <label htmlFor="billingAddress" className="form-label">
+              Billing Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="billingAddress"
+              placeholder="123 Main St"
+            />
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="billingCity" className="form-label">
+                  City
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="billingCity"
+                  placeholder="New York"
+                />
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="input-group">
+                <label htmlFor="billingZip" className="form-label">
+                  ZIP Code
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="billingZip"
+                  placeholder="10001"
+                />
+              </div>
             </div>
           </div>
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="cvv" className="form-label">
-                CVV
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="cvv"
-                placeholder="123"
-                // required
-              />
-            </div>
+          <div className="ccContainer">
+            <button type="submit" className="submit-button">
+              Submit Payment
+            </button>
           </div>
+        </form>
+      ) : (
+        <div className="confirmation-message">
+          Your gift has been received.
         </div>
-
-        {/* Billing Information */}
-        <div className="input-group">
-          <label htmlFor="billingAddress" className="form-label">
-            Billing Address
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="billingAddress"
-            placeholder="123 Main St"
-            // required
-          />
-        </div>
-
-        <div className="row">
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="billingCity" className="form-label">
-                City
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="billingCity"
-                placeholder="New York"
-                // required
-              />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="input-group">
-              <label htmlFor="billingZip" className="form-label">
-                ZIP Code
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="billingZip"
-                placeholder="10001"
-                // required
-              />
-            </div>
-          </div>
-        </div>
-        <div className="ccContainer">
-          <button type="submit" className="submit-button">
-            Submit Payment
-          </button>
-        </div>
-      </form>
+      )}
     </div>
   );
 }
--- open-hearts-front-end/src/components/FamilyDetails.jsx
+++ open-hearts-front-end/src/components/FamilyDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import SingleCreditCardForm from './SingleCreditCardForm'; // Import the updated SingleCreditCardForm component
+import SingleCreditCardForm from './SingleCreditCardForm'; 
 
 function FamilyDetails() {
   const { id } = useParams();
