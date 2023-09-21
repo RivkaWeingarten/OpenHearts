c592ea259815317f69958271f534282e0b968df2
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-09-21 16:58:24 -0700

    updated home css

--- open-hearts-front-end/src/components/Home.jsx
+++ open-hearts-front-end/src/components/Home.jsx
@@ -6,13 +6,13 @@ function Home() {
       <div className="aboutUs">
         <h1>About Us</h1>
         <img className='family'  src="https://img.freepik.com/free-vector/big-family-meeting_74855-5220.jpg?" alt='illustration of family'></img>
-        <p>
+        <p className="mission">
           We are a community organization with a mission to aid families
           struggling and in need of financial assistance. Our philosphy focusus
           on helping single-mothers acquire financial assistance but it doesn't
           end there. With the help of your contributions we can fund financial
           literacy and education for these lovely families. Help us in our
-          mission to end poverty. You can select one or as many as you'd like
+          mission to end poverty. You can select one or as many as you would like
           from our list of families. Click "Get Involved" below to get to know our
           families and make your contributions.
         </p>
